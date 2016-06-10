27c4b3382ea0d38bb5a0736d17124ec093c42c71
prashant pandey <prashantsundu@gmail.com>
2016-06-10 14:24:44 +0530

    Create fourDigitSum.java

---
+++ misc/fourDigitSum.java
@@ -0,0 +1,43 @@
+package Code;
+
+
+
+
+public class Solution {
+
+
+    public static int fourDigitSum(String digit)
+    {
+        int sum= 0;
+        int mf = 1000;
+        String strArr[] = digit.split(" ");
+
+        while (mf!= 0)
+        {
+            for(int i=0; i< strArr.length;i++)
+            {
+                if(!(strArr[i]=="0" && mf==1000))
+                sum += (Integer.parseInt(strArr[i])*mf);
+            }
+            mf /= 10;
+
+        }
+
+
+         return sum;
+    }
+
+
+
+
+    public static void main(String args[]){
+
+        String str = new String("4021");
+        System.out.println(fourDigitSum(str));
+
+    }
+
+
+
+
+}
