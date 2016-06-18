4efbe8787e5837270b362ff77da3d738a3a1b880
prashant pandey <prashantsundu@gmail.com>
2016-06-18 17:18:01 +0530

    Create DivideWithoutItsOperator.java

---
+++ ArithmeticWithConstraint/DivideWithoutItsOperator.java
@@ -0,0 +1,43 @@
+package Code;
+import java.util.*;
+
+public class Solution {
+
+    public static int divide(int divident,int divisor){
+
+        if(divisor==0)
+            return -1;
+
+        if(divident < divisor)
+            return  0;
+
+        if(divident == divisor)
+            return  1;
+
+
+        int current = 1;
+        int den = divisor;
+
+        while (divident > den)
+        {
+            den = den <<1;
+            current = current<<1;
+        }
+
+        den = den>>1;
+        current = current>>1;
+
+
+        return current + divide(divident-den,divisor);
+    }
+
+
+    public static void main(String args[]){
+        Integer a = 12;
+        //System.out.println(Integer.toBinaryString(a));
+        //System.out.println(a&1);
+        System.out.println(divide(21,15));
+
+    }
+
+}
