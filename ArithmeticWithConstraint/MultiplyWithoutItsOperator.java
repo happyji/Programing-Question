f2a560c61983aa74046c6ad4c55a6e84193a35b4
prashant pandey <prashantsundu@gmail.com>
2016-06-18 17:19:34 +0530

    Create MultiplyWithoutItsOperator.java

---
+++ ArithmeticWithConstraint/MultiplyWithoutItsOperator.java
@@ -0,0 +1,29 @@
+package Code;
+import java.util.*;
+
+public class Solution {
+
+    public static int multiply(int a,int b){
+        int ans = 0;
+        while (a != 0){
+
+            if((a & 1) != 0){
+                ans += b;
+            }
+
+            a >>= 1;
+            b <<= 1;
+        }
+        return ans;
+
+    }
+
+
+    public static void main(String args[]){
+        Integer a = 12;
+        //System.out.println(Integer.toBinaryString(a));
+        //System.out.println(a&1);
+        System.out.println(multiply(21,15));
+    }
+
+}
