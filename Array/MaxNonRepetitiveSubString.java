352eeb3a3c2d2ea05064fee76ee28ef73fdf39d0
prashant pandey <prashantsundu@gmail.com>
2016-08-15 18:18:38 +0530

    Create MaxNonRepetitiveSubString.java

---
+++ Array/MaxNonRepetitiveSubString.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+
+class Solution
+{
+
+
+    static int getMaxNonRepetitiveChar(String str)
+    {
+        HashMap<Character,Integer> map = new HashMap<>();
+        map.put(str.charAt(0),0);
+        int result=-1;
+
+        int i=1;
+        int max =1;
+        int startIndex =0;
+        while (i< str.length())
+        {
+            Character currentChar = str.charAt(i);
+            if(map.containsKey(currentChar))
+            {
+                if(map.get(currentChar) >= startIndex)
+                {
+                    map.replace(currentChar,i);
+                    startIndex = i;
+                    if(max > result)
+                        result = max;
+
+                    max =1;
+                }
+            }
+            else
+            {
+                map.put(currentChar,i);
+                max++;
+            }
+            i++;
+        }
+
+
+        return result;
+    }
+
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        System.out.println(getMaxNonRepetitiveChar("GEEKSFORGEEKS"));
+    }
+}
